fef11073f40923e2cb5220336d838511446e0b70
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2023-12-11 10:48:35 +0100

    Inicio

---
+++ app/src/main/java/com/exam/rodrigo_hernandez/MainActivity.java
@@ -0,0 +1,19 @@
+package com.exam.rodrigo_hernandez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView fondo = findViewById(R.id.backgroundSplash);
+
+
+    }
+}
