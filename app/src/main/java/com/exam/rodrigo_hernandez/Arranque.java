b763e39909ad4a2ea0863ff64646974216414fb5
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2023-12-11 12:17:36 +0100

    Imagen y logo done

--- app/src/main/java/com/exam/rodrigo_hernandez/Arranque.java
+++ app/src/main/java/com/exam/rodrigo_hernandez/Arranque.java
@@ -33,8 +33,9 @@ public class Arranque extends AppCompatActivity {
 
 
         ImageView fondo = findViewById(R.id.backgroundSplash);
+        fondo.setAlpha(0.2f);
         Glide.with(this)
-                .load("https://images.unsplash.com/photo-1532664189809-02133fee698d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .load(R.drawable.fondo)
                 .transition(DrawableTransitionOptions.withCrossFade(2000))
                 .centerCrop()
                 .into(fondo);
